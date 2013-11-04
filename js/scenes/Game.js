79fa8b3db1803b3700cb6560f111beb82a6637b9
billnreed <billnreed@gmail.com>
2013-11-03 17:28:41 -0800

    Platforms are continuously spawned from a PlatformSpawner

--- js/scenes/Game.js
+++ js/scenes/Game.js
@@ -1,12 +1,9 @@
-define(["crafty", "components/moves-straight", "components/platform"], function() {
+define(["crafty", "components/moves-straight", "components/platform", "spawners/platform-spawner"], function() {
     Crafty.scene("Game", function() {
         Crafty.background("#73a6e2");
         
-        Crafty.e("Platform")
-            .platform(100, 600, 2, 500, 20);
-        
-        Crafty.e("Platform")
-            .platform(700, 600, 2, 500, 20);
+        Crafty.e("PlatformSpawner")
+            .platformSpawner();
 
             
         Crafty.e("2D, Canvas, Color, Gravity, Twoway")
---
+++ tests/spawners-spec/platform-spawner-spec.js
@@ -0,0 +1,5 @@
+define(["crafty", "spawners/platform-spawner"], function(Crafty) {
+    describe("A PlatformSpawner", function() {
+        
+    });
+});
