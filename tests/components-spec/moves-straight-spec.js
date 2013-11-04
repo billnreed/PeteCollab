64f95f0f3bedaf02f4b99f11ad7642f3b812a928
billnreed <billnreed@gmail.com>
2013-11-03 17:12:29 -0800

    updated MovesStraight unit tests
    still more to do

--- tests/components-spec/moves-straight-spec.js
+++ tests/components-spec/moves-straight-spec.js
@@ -1,27 +1,36 @@
 define(["crafty", "components/moves-straight"], function(Crafty) {
-    xdescribe("A MovesStraight component", function() {
-        var ms;
-        
-        beforeEach(function() {
-            var sourceX = 0;
-            var sourceY = 0;
-            var targetX = 10;
-            var targetY = 0;
-            var speed = 2;
-            ms = Crafty.e("MovesStraight, 2D")
-                .attr({x: sourceX, y: sourceY})
-                .movesStraight(targetX, targetY, speed);
-        });
-        
-        it("moves towards its target", function() {
-            Crafty.timer.step();
+    describe("A MovesStraight component", function() {
+        describe("moving right", function() {
+            var ms;
             
-            expect(ms.x).toBe(2);
-            expect(ms.y).toBe(0);
-        });
-        
-        it("moves at the given speed", function() {
+            beforeEach(function() {
+                var sourceX = 0;
+                var sourceY = 0;
+                var targetX = 10;
+                var targetY = 0;
+                var speed = 2;
+                ms = Crafty.e("MovesStraight")
+                    .attr({x: sourceX, y: sourceY})
+                    .movesStraight(targetX, targetY, speed);
+            });
             
-        });
+            afterEach(function() {
+                ms.destroy();
+            })
+            
+            it("moves towards its target", function() {
+                Crafty.timer.simulateFrames(1);
+                
+                expect(ms.x).toBe(2);
+                expect(ms.y).toBe(0);
+            });
+            
+            it("moves at the given speed", function() {
+                Crafty.timer.simulateFrames(5);
+                
+                expect(ms.x).toBe(10);
+                expect(ms.y).toBe(0);
+            });  
+        })
     });
 });
