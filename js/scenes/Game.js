9ca13e66efbe04ddcadae220bb4b64de1b621fd0
billnreed <billnreed@gmail.com>
2013-11-03 18:30:53 -0800

    Added a MovesStraight component for upcoming Bullet component

--- js/scenes/Game.js
+++ js/scenes/Game.js
@@ -1,9 +1,12 @@
-define(["Crafty", "Spawners/PlatformSpawner", "Entities/Player"], function(Crafty) {
+define(["Crafty", "Spawners/PlatformSpawner", "Entities/Player", "Entities/Bullet"], function(Crafty) {
     Crafty.scene("Game", function() {
         Crafty.background("#73a6e2");
         
         Crafty.e("PlatformSpawner")
             .platformSpawner();
+        
+        Crafty.e("Bullet")
+            .bullet(100, 100, "right", 3);
         
         Crafty.e("Player").player();
     });
