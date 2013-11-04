778880434e5f440946fd1ef5bd63148560cad27b
billnreed <billnreed@gmail.com>
2013-11-03 23:05:46 -0800

    SpeedTokens now speed up the player

--- poc2/js/Entities/Player.js
+++ poc2/js/Entities/Player.js
@@ -4,15 +4,20 @@ define(["Crafty"], function(Crafty) {
         _height: 30,
         _playerColor: "#19f2e8",
         
-        _speedTokens: 0,
+        _left: false,
+        _right: false,
+        
+        _speedTokens: 1,
         
         player: function(playerX, playerY) {
-            this.requires("2D, Canvas, Color, Gravity, Twoway, Collision")
+            this.requires("2D, Canvas, Color, Gravity, Keyboard, Collision")
                 .attr({x: playerX, y: playerY, w: 30, h: 30})
                 .color(this._playerColor)
                 .gravity("Floor")
-                .twoway(3, 0)
-                .onHit("SpeedToken", this._collectSpeedToken);
+                .onHit("SpeedToken", this._collectSpeedToken)
+                .bind("KeyDown", this._handleKeyPress)
+                .bind("KeyUp", this._handleKeyPress)
+                .bind("EnterFrame", this._move);
             
             return this;
         },
@@ -25,5 +30,20 @@ define(["Crafty"], function(Crafty) {
             
             Crafty.trigger("COLLECTED_SPEED_TOKEN", this._speedTokens);
         },
+        
+        _handleKeyPress: function() {            
+            this._left = this.isDown(Crafty.keys.A);
+            this._right = this.isDown(Crafty.keys.D);
+        },
+        
+        _move: function() {
+            if (this._left) {
+                this.x -= this._speedTokens;
+            }
+            
+            if (this._right) {
+                this.x += this._speedTokens;
+            }
+        }
     });
 });
