860999327bc315a2507d6bb4f852738f105f45aa
billnreed <billnreed@gmail.com>
2013-11-16 18:04:12 -0800

    Levels are now read from Json objects

--- poc1/js/Entities/FinalGoal.js
+++ poc1/js/Entities/FinalGoal.js
@@ -3,11 +3,13 @@ define(["Crafty", "Entities/Goal"], function(Crafty) {
         _hasHit: false,
 
         finalGoal: function(x, y, revealAmount) {
-            this.requires("Goal, Collision")
-                .goal(x, y, revealAmount)
+            this.requires("2D, Canvas, Color, Collision")
+                .attr({x: x, y: y, w: 10, h: 15})
+                .color("#f0f")
+                .bind("EnterFrame", this._onHit);
         },        
         
-        //override Goal._onHit
+        
         _onHit: function() {
             if (!this._hasHit && this.hit("Player")) {
                 Crafty("Screen").each(function() {
--- poc1/js/Scenes/Game.js
+++ poc1/js/Scenes/Game.js
@@ -1,34 +1,62 @@
 define(["Crafty", "Entities/Screen", "Entities/Platform", "Entities/Player", "Entities/Goal", "Entities/FinalGoal"], function(Crafty, directions) {
+    var levelData = {
+        player: {x: 100, y: 260},
+        platforms: [
+            {x: 30, y: 150, width: 200, height: 10},
+            {x: 20, y: 200, width: 30, height: 10},
+            {x: 20, y: 250, width: 40, height: 10},
+            {x: 50, y: 300, width: 70, height: 10},
+            {x: 230, y: 500, width: 30, height: 10}
+        ],
+        goals: [
+            {x: 60, y: 285, revealAmount: 50},
+            {x: 30, y: 185, revealAmount: 50},
+            {x: 200, y: 135, revealAmount: 60}
+        ],
+        finalGoal: {x: 240, y: 485},
+        topScreen: {direction: directions.up},
+        bottomScreen: {direction: directions.down},
+        initialRevealAmount: 60
+    };
+    
     Crafty.scene("Game", function() {
         Crafty.background("#0dd0ff");
         
-        var player = Crafty.e("Player").player(100, 260);
-    
-        Crafty.e("Platform")
-            .platform(30, 150, 200, 10);
-        Crafty.e("Platform")
-            .platform(20, 200, 30, 10);
-        Crafty.e("Platform")
-            .platform(20, 250, 40, 10);
-        Crafty.e("Platform")
-            .platform(50, 300, 70, 10);
-        Crafty.e("Platform")
-            .platform(230, 500, 30, 10);
+        var level = createLevel(levelData);
         
-        Crafty.e("Goal")
-            .goal(60, 285, 50);
-        Crafty.e("Goal")
-            .goal(30, 185, 50);
-        Crafty.e("Goal")
-            .goal(200, 135, 60);
+        var initialRevealAmount = level.initialRevealAmount;
+        level.topScreen.reveal(initialRevealAmount);
+        level.bottomScreen.reveal(initialRevealAmount);
+    });
+    
+    var createLevel = function(levelData) {
+        var player = Crafty.e("Player").player(levelData.player.x, levelData.player.y);
+            
+        for (var i = 0; i < levelData.platforms.length; i++) {
+            var platformData = levelData.platforms[i];
+            Crafty.e("Platform")
+                .platform(platformData.x, platformData.y, platformData.width, platformData.height);
+        }
+        
+        for (var i = 0; i < levelData.goals.length; i++) {
+            var goalData = levelData.goals[i];
+            Crafty.e("Goal")
+                .goal(goalData.x, goalData.y, goalData.revealAmount);
+        }
+        
         Crafty.e("FinalGoal")
-            .finalGoal(240, 485, 80);
+            .finalGoal(levelData.finalGoal.x, levelData.finalGoal.y);
         
         var topScreen = Crafty.e("Screen")
-            .screen(directions.up);
+            .screen(levelData.topScreen.direction);
         var bottomScreen = Crafty.e("Screen")
-            .screen(directions.down);
-        topScreen.reveal(60);
-        bottomScreen.reveal(60);
-    });
+            .screen(levelData.bottomScreen.direction);
+        var initialRevealAmount = levelData.initialRevealAmount;
+        
+        return {
+            topScreen: topScreen,
+            bottomScreen: bottomScreen,
+            initialRevealAmount: initialRevealAmount
+        };
+    }
 });
